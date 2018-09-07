2aca0c3d2a0bab296908cd8d938ca4a6e5579c1d
kylelingat <kylejlingat@gmail.com>
2018-09-07 10:45:34 -1000

    node

--- js/app.js
+++ js/app.js
@@ -2,34 +2,37 @@ console.log('hello')
 // you will also have to setup the referring domains on your marvel developer portal
 var PRIV_KEY = "efd5b4b98dcfbf76bb31fe576d0b824d1876ca3d";
 var PUBLIC_KEY = "74147ee7b91d932edc968cb4cbbbdd1e";
+var ts = new Date().getTime();
+var hash = CryptoJS.MD5(ts + PRIV_KEY + PUBLIC_KEY).toString();
+var url = 'http://gateway.marvel.com:80/v1/public/characters';
 
-function getMarvelResponse() {
+function getMarvelResponse(charName) {
+    console.log(url);
+    $.getJSON(url, {
+            ts: ts,
+            apikey: PUBLIC_KEY,
+            hash: hash,
+            name: charName
+        })
+        .done(function(data) {
+            // sort of a long dump you will need to sort through
+            console.log(data)
+            console.log(data.data.results[0].thumbnail);
 
-  // you need a new ts every request
-  var ts = new Date().getTime();
-  var hash = CryptoJS.MD5(ts + PRIV_KEY + PUBLIC_KEY).toString();
+            var placeName = document.createElement("h1");
+            var grabbedName = document.createTextNode(data.data.results[0].name);
 
+            var imgHolder = document.createElement("IMG");
+            imgHolder.setAttribute("src", data.data.results[0].thumbnail.path + "/standard_fantastic.jpg");
 
-
-  var url = 'http://gateway.marvel.com:80/v1/public/characters';
-
-  console.log(url);
-  $.getJSON(url, {
-    ts: ts,
-    apikey: PUBLIC_KEY,
-    hash: hash,
-    name: "Thor"
-    })
-    .done(function(data) {
-      // sort of a long dump you will need to sort through
-      console.log(data.data.results[0].thumbnail);
-      $("#test").text(data.data.results[0].name)
-      $("#my_image").attr("src",data.data.results[0].thumbnail.path + "/standard_fantastic.jpg");
-    })
-    .fail(function(err){
-      // the error codes are listed on the dev site
-      console.log(err);
-    });
+            placeName.appendChild(grabbedName);
+            document.getElementById("container").appendChild(placeName);
+        })
+        .fail(function(err) {
+            // the error codes are listed on the dev site
+            console.log(err);
+        });
 };
 
-getMarvelResponse();
+getMarvelResponse("Captain America");
+getMarvelResponse("Thor");
